f29e049fd2dc771bb60cdc85b5c93935254ded12
Joe Young <joeyoungwx@gmail.com>
2011-07-29 01:07:18 -0600

    Removed precompiled files since all versions were older. Now only un-compiled code remains.

--- streams/format/ncar_vaisala_ceilometer/algorithms/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# this is the init script for the algorithms package. This code will be used to help manage algorithms.
-
-def surf_pol(*args,**kwargs):
-	from streams.format.ncar_vaisala_ceilometer.algorithms.sfc_pol_6 import alg as sp
-	return sp(*args,**kwargs)
-	
